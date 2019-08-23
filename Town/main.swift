8857e2fbbfbf1afeb82ebd0dfc65adc7441f822c
ShelbyCohen <s.cohen131@gmail.com>
2019-08-23 11:24:26 -0700

    Main

---
+++ Town/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Town
+//
+//  Created by Cohen, Shelby on 8/23/19.
+//  Copyright © 2019 Cohen, Shelby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
